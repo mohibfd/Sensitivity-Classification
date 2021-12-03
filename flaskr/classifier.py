d1ae1e606c8c38423a5fd96a645e1abe2727962c
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-12-03 20:41:25 +0000

    implemented cross validation to see all documents

--- flaskr/classifier.py
+++ flaskr/classifier.py
@@ -7,25 +7,20 @@ from flaskr.auth import login_required
 from flaskr.db import get_db
 
 import os
-import sys
-import numpy as np
 import pandas as pd
-import sklearn.ensemble
-import sklearn.pipeline
-from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 from lime.lime_text import LimeTextExplainer
+from sklearn.model_selection import cross_val_predict
+from sklearn.model_selection import cross_val_score
+import numpy as np
 
 bp = Blueprint('classifier', __name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 MODEL_PATH = os.path.join(APP_ROOT, "model/")
 
-train_data = pd.read_pickle(MODEL_PATH + 'train_data.pkl')
-test_data = pd.read_pickle(MODEL_PATH + 'test_data.pkl')
-
-train_labels = train_data['Sensitive']
-test_labels = test_data['Sensitive']
+cross_val_stats = pd.read_pickle(MODEL_PATH + 'cross_val_stats.pkl')
+main_data = pd.read_pickle(MODEL_PATH + 'main_data.pkl')
 
 
 @bp.route('/')
@@ -48,87 +43,66 @@ def non_sensitive_info():
 @bp.route('/general-sensitivity-info')
 @login_required
 def general_sensitivity_info():
+    X = main_data['features']
+    y = main_data['labels']
+    model = main_data['classifier']
 
-    model = LogisticExplainer()
-    lr_predict = model.predict(test_data['Body'], probability=False)
+    scores = cross_val_score(model, X, y, cv=5, scoring="f1_micro")
+    prediction = "f1: %0.2f (+/- %0.2f)" % (np.mean(scores), np.std(scores))
 
-    prediction = "%.3f" % (f1_score(
-        lr_predict, test_labels, average="macro"))
-
-    confusion_matrix_score = confusion_matrix(test_labels, lr_predict)
+    y_pred = cross_val_predict(model, X, y, cv=5)
+    confusion_matrix_score = confusion_matrix(y, y_pred)
 
     return render_template('classifier/general_sensitivity_info.html', prediction=prediction, confusion_matrix_score=confusion_matrix_score)
 
 
-class LogisticExplainer:
-    """Class to explain classification results of a scikit-learn
-       Logistic Regression Pipeline. The model is trained within this class.
-    """
-
-    def __init__(self) -> None:
-        "Input training data path for training Logistic Regression classifier"
-        import pandas as pd
-        # Read in training data set
-        self.train_df = train_data
-        self.train_df['truth'] = train_labels
-        # Categorical data type for truth labels
-        self.train_df['truth'] = self.train_df['truth'].astype(
-            int).astype('category')
-
-    def train(self) -> sklearn.pipeline.Pipeline:
-        "Create sklearn logistic regression model pipeline"
-        from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
-        from sklearn.linear_model import LogisticRegression
-        from sklearn.pipeline import Pipeline
-        pipeline = Pipeline(
-            [
-                ('vect', CountVectorizer(binary=True)),
-                ('tfidf', TfidfTransformer()),
-                ('clf', LogisticRegression()),
-            ]
-        )
-        # Train model
-        classifier = pipeline.fit(
-            self.train_df['Body'], self.train_df['truth'])
-        return classifier
-
-    def predict(self, texts, probability=True) -> np.array([float, ...]):
-        """Generate an array of predicted scores (probabilities) from sklearn
-        Logistic Regression Pipeline."""
-        classifier = self.train()
-
-        if probability:
-            probs = classifier.predict_proba(texts)
-        else:
-            probs = classifier.predict(texts)
-        return probs
-
-
 def explainer(document_number) -> LimeTextExplainer:
     """Run LIME explainer on provided classifier"""
+    index = 0
+    fold1_position = len(cross_val_stats["test_labels_list"][0])
+    fold2_position = len(
+        cross_val_stats["test_labels_list"][1]) + fold1_position
+    fold3_position = len(
+        cross_val_stats["test_labels_list"][2]) + fold2_position
+    fold4_position = len(
+        cross_val_stats["test_labels_list"][3]) + fold3_position
 
-    model = LogisticExplainer()
-    predictor = model.predict
+    if document_number < fold1_position:
+        index = 0
+    elif document_number < fold2_position:
+        index = 1
+        document_number = document_number - fold1_position
+    elif document_number < fold3_position:
+        index = 2
+        document_number = document_number - fold2_position
+    elif document_number < fold4_position:
+        index = 3
+        document_number = document_number - fold3_position
+    else:
+        index = 4
+        document_number = document_number - fold4_position
 
-    # Create a LimeTextExplainer
-    explainer = LimeTextExplainer(
-        class_names=['Non-Sensitive', 'Sensitive']
-    )
+    text_data = cross_val_stats["test_features_list"][index]
+    one_hot_vectorizer = cross_val_stats["vectorizers"][index]
+    model = cross_val_stats["classifiers"][index]
 
-    data = test_data['Body']
+    def predictor(texts):
+        feature = one_hot_vectorizer.transform(texts)
+        pred = model.predict_proba(feature)
+        return pred
+
+    explainer = LimeTextExplainer(class_names=['Non-Sensitive', 'Sensitive'])
 
     first_idx = document_number
     second_idx = first_idx + 1
 
-    specific_data = data[first_idx:second_idx].iloc[0]
+    specific_data = text_data[first_idx:second_idx].iloc[0]
     specific_data = specific_data[0:300]
 
     # Make a prediction and explain it:
     exp = explainer.explain_instance(
         specific_data,
         classifier_fn=predictor,
-        top_labels=1,
-        num_features=4,
     )
     return exp
 
@@ -147,16 +121,16 @@ def single_document_sensitivity_info():
 
     if request.method == 'POST':
 
-        print('Hello world!>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', file=sys.stderr)
-        print(document_number, file=sys.stderr)
-
         if request.form['submit_button'] == 'Previous Document':
             if (document_number == 0):
                 flash("There are no previous documents")
             else:
                 document_number -= 1
         elif request.form['submit_button'] == 'Next Document':
-            document_number += 1
+            if (document_number == 3800):
+                flash("There are no more documents")
+            else:
+                document_number += 1
         db.execute(
             'UPDATE user SET document_number = ?'
             ' WHERE id = ?',
@@ -167,4 +141,4 @@ def single_document_sensitivity_info():
     exp = explainer(document_number)
     exp = exp.as_html()
 
-    return render_template('classifier/single_document_sensitivity_info.html', exp=exp)
+    return render_template('classifier/single_document_sensitivity_info.html', exp=exp, document_number=document_number+1)
