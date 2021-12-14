cb7f5373cb66bde1665e7dcc9d48e23974ec848f
Mohib Akoum <2431135A@student.gla.ac.uk>
2021-12-14 19:29:49 +0000

    fixing broken tests and adding new tests

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -20,14 +20,31 @@ def test_paths_logged_in(client, path, auth):
     assert client.get(path).status_code == 200
 
 
-@pytest.mark.parametrize(('document_num', 'message', 'button_action'), (
+documents_visibility_list = [
     (0, b'There are no previous documents', 'Previous Document'),
-    (500, b'document number =\n  500', 'Previous Document'),
-    (500, b'document number =\n  501', ''),
-    (500, b'document number =\n  502', 'Next Document'),
-    (3800, b'There are no more documents', 'Next Document'),
-))
-def test_documents_visibility(client, app, auth, document_num, message, button_action):
+    (543, b'Document number =\n    543', 'Previous Document'),
+    (543, b'Document number =\n    544', ''),
+    (543, b'Document number =\n    545', 'Next Document'),
+]
+
+all_docs_visib = documents_visibility_list.copy()
+all_docs_visib.append(
+    (3800, b'There are no more documents', 'Next Document')
+)
+
+sens_docs_visib = documents_visibility_list.copy()
+sens_docs_visib.append(
+    (501, b'There are no more documents', 'Next Document')
+)
+
+non_sens_docs_visib = documents_visibility_list.copy()
+non_sens_docs_visib.append(
+    (3298, b'There are no more documents', 'Next Document')
+)
+
+
+@pytest.mark.parametrize(('document_num', 'message', 'button_action'), all_docs_visib)
+def test_single_documents_visibility(client, app, auth, document_num, message, button_action):
     auth.login()
     with app.app_context():
         db = get_db()
@@ -38,6 +55,42 @@ def test_documents_visibility(client, app, auth, document_num, message, button_a
 
     response = client.post(
         '/single-document-sensitivity-info',
+        data={'submit_button': button_action}
+    )
+
+    assert message in response.data
+
+
+@pytest.mark.parametrize(('document_num', 'message', 'button_action'), sens_docs_visib)
+def test_sensitive_documents_visibility(client, app, auth, document_num, message, button_action):
+    auth.login()
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE user SET sens_document_number = ? WHERE id = 1',
+                   (document_num,)
+                   )
+        db.commit()
+
+    response = client.post(
+        '/sensitive-info',
+        data={'submit_button': button_action}
+    )
+
+    assert message in response.data
+
+
+@pytest.mark.parametrize(('document_num', 'message', 'button_action'), non_sens_docs_visib)
+def test_non_sensitive_documents_visibility(client, app, auth, document_num, message, button_action):
+    auth.login()
+    with app.app_context():
+        db = get_db()
+        db.execute('UPDATE user SET non_sens_document_number = ? WHERE id = 1',
+                   (document_num,)
+                   )
+        db.commit()
+
+    response = client.post(
+        '/non-sensitive-info',
         data={'submit_button': button_action}
     )
 
