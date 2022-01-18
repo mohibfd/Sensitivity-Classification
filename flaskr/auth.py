6aad56ee5934fd9948eb9515cf8914354ed91e12
Mohib Akoum <2431135A@student.gla.ac.uk>
2022-01-18 18:17:42 +0000

    improved login page

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -47,6 +47,10 @@ def register():
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
+
+        if request.form.get('go_sign_up'):
+            return redirect(url_for("auth.register"))
+
         username = request.form['username']
         password = request.form['password']
         db = get_db()
