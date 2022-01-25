19d05dd7384f3654421b8ff383e4377a2f41d838
Mohib Akoum <2431135A@student.gla.ac.uk>
2022-01-25 23:44:21 +0000

    removing blog code since not used anymore

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -11,12 +11,3 @@ CREATE TABLE user (
   visualisation_method VARCHAR NOT NUlL,
   clf_method VARCHAR NOT NUlL
 );
-
--- CREATE TABLE post (
---   id INTEGER PRIMARY KEY AUTOINCREMENT,
---   author_id INTEGER NOT NULL,
---   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
---   title TEXT NOT NULL,
---   body TEXT NOT NULL,
---   FOREIGN KEY (author_id) REFERENCES user (id)
--- );
